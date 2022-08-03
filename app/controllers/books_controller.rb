261f8fbac9c799015a2077531f2858dbb2554a09
EC2 Default User <ec2-user@ip-172-31-11-23.us-west-1.compute.internal>
2022-08-03 09:48:00 +0000

    [add] bookのindexページ　userのshowページ作成（途中）

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,12 +2,29 @@ class BooksController < ApplicationController
   def new
   end
 
+  def create
+    @book=Book.new(book_params)
+    @book.user_id=current_user.id
+    @book.save
+     redirect_to book_path(@book.id)
+
+  end
+
   def index
+    @book=Book.new
+    @books=Book.all
+    @user=current_user
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
-
-  resources :books,only: [:new,:index,:show,:edit]
   root to: 'homes#top'  #/
   get"homes/about" => "homes#about",as: "about"
+
+  resources :books,only: [:new,:create,:index,:show,:edit]
+  resources :users,only: [:show,:edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
