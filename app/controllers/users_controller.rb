420dd1baa12955c79ec3f68b6284b0018db6dcd4
EC2 Default User <ec2-user@ip-172-31-9-38.us-west-1.compute.internal>
2022-08-06 05:33:19 +0000

    [add]userのindexページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,16 @@ class UsersController < ApplicationController
     @book.save
      redirect_to book_path(@book.id)
    end
+   
+   def index
+    @users=User.all
+    @user=current_user
+    @book=Book.new
+   end 
+   
+     
 
-  def show
+   def show
     @user=User.find(params[:id])
     @books=@user.books
     @book=Book.new
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'  #/
   get"homes/about" => "homes#about",as: "about"
 
-  resources :books
-  resources :users
+  resources :books,only: [:new,:create,:index,:show,:edit,:update,:destroy]
+  resources :users,only: [:show,:index,:edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
